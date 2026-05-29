e638c2e6c22e83c38ce7ec1da7e0ca6a5b8f0750
ehs03y3ol <soed.w3c@gmail.com>
2026-05-29 06:14:13 +0200

    Refactor scraper.bat to improve clarity and functionality
    Removed unnecessary comments and updated curl commands to include user-agent.

--- scraper.bat
+++ scraper.bat
@@ -1,7 +1,4 @@
-:: This is 100% made with IA, this is 4:42 AM I have insomnia and I don't want to heat my brain anymore
-
 @echo off
-:: This line is mandatory so we can create and read the ID variable inside the loop!
 setlocal EnableDelayedExpansion
 
 set "witRute=C:\wit-v3.05a-r8638-cygwin64\bin\wit.exe"
@@ -17,13 +14,12 @@ if not exist "%witRute%" (
 echo Ready to extract Game IDs and download covers from GameTDB!
 pause
 
-:: 1. Create the folder structure for USB Loader GX
+:: Create the folder structure
 if not exist "%imgFolder%\2D" mkdir "%imgFolder%\2D"
 if not exist "%imgFolder%\3D" mkdir "%imgFolder%\3D"
 if not exist "%imgFolder%\disc" mkdir "%imgFolder%\disc"
 if not exist "%imgFolder%\full" mkdir "%imgFolder%\full"
 
-:: 2. Check if the games folder has CISO files
 if exist "games\*.ciso" (
     
     for %%F in ("games\*.ciso") do (
@@ -32,36 +28,36 @@ if exist "games\*.ciso" (
         echo Processing: "%%~nxF"
         echo ----------------------------------------------------
         
-        :: 3. Extract the 6-character ID using wit and save to !gameID!
-        for /f "delims=" %%I in ('"%witRute%" id6 "%%~fF"') do set "gameID=%%I"
-        echo Found Game ID: !gameID!
+        rem Extract the raw ID
+        for /f "delims=" %%I in ('""%witRute%" id6 "%%~fF""') do set "rawID=%%I"
         
-        :: 4. Download 2D Cover (Try US first, if not found, fallback to EN)
+        rem Force exactly 6 characters to kill the invisible carriage return!
+        set "gameID=!rawID:~0,6!"
+        echo Found Game ID: [!gameID!]
+        
+        rem Using the correct /wii/ directory with Anti-Bot bypass (-L -A)
         echo Downloading 2D Cover...
-        curl -f -s -o "%imgFolder%\2D\!gameID!.png" "https://art.gametdb.com/wii/cover/US/!gameID!.png"
+        curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\2D\!gameID!.png" "https://art.gametdb.com/wii/cover/US/!gameID!.png"
         if not exist "%imgFolder%\2D\!gameID!.png" (
-            curl -f -s -o "%imgFolder%\2D\!gameID!.png" "https://art.gametdb.com/wii/cover/EN/!gameID!.png"
+            curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\2D\!gameID!.png" "https://art.gametdb.com/wii/cover/EN/!gameID!.png"
         )
         
-        :: Download 3D Cover
         echo Downloading 3D Cover...
-        curl -f -s -o "%imgFolder%\3D\!gameID!.png" "https://art.gametdb.com/wii/cover3D/US/!gameID!.png"
+        curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\3D\!gameID!.png" "https://art.gametdb.com/wii/cover3D/US/!gameID!.png"
         if not exist "%imgFolder%\3D\!gameID!.png" (
-            curl -f -s -o "%imgFolder%\3D\!gameID!.png" "https://art.gametdb.com/wii/cover3D/EN/!gameID!.png"
+            curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\3D\!gameID!.png" "https://art.gametdb.com/wii/cover3D/EN/!gameID!.png"
         )
         
-        :: Download Disc Art
         echo Downloading Disc Artwork...
-        curl -f -s -o "%imgFolder%\disc\!gameID!.png" "https://art.gametdb.com/wii/disc/US/!gameID!.png"
+        curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\disc\!gameID!.png" "https://art.gametdb.com/wii/disc/US/!gameID!.png"
         if not exist "%imgFolder%\disc\!gameID!.png" (
-            curl -f -s -o "%imgFolder%\disc\!gameID!.png" "https://art.gametdb.com/wii/disc/EN/!gameID!.png"
+            curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\disc\!gameID!.png" "https://art.gametdb.com/wii/disc/EN/!gameID!.png"
         )
         
-        :: Download Full Cover Wrap
         echo Downloading Full Cover...
-        curl -f -s -o "%imgFolder%\full\!gameID!.png" "https://art.gametdb.com/wii/coverfull/US/!gameID!.png"
+        curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\full\!gameID!.png" "https://art.gametdb.com/wii/coverfull/US/!gameID!.png"
         if not exist "%imgFolder%\full\!gameID!.png" (
-            curl -f -s -o "%imgFolder%\full\!gameID!.png" "https://art.gametdb.com/wii/coverfull/EN/!gameID!.png"
+            curl -f -L -s -A "Mozilla/5.0" -o "%imgFolder%\full\!gameID!.png" "https://art.gametdb.com/wii/coverfull/EN/!gameID!.png"
         )
         
         echo [SUCCESS] Finished downloads for !gameID!
@@ -75,7 +71,4 @@ if exist "games\*.ciso" (
 echo.
 echo ====================================================
 echo Script finished! 
-echo You can now copy the 4 folders inside "%imgFolder%" 
-echo directly to your SD card inside "apps\usbloader_gx\images\"
-echo ====================================================
 pause
