d0807994b6f529ea8e5b1d39718a66079e47c978
ehs03y3ol <soed.w3c@gmail.com>
2026-05-29 03:57:17 +0200

    Refactor iso2ciso.bat for better error handling
    Improved error handling and user prompts for missing ISO files and output directory.

--- iso2ciso.bat
+++ iso2ciso.bat
@@ -1,39 +1,45 @@
 @echo off
-set witRute=C:\wit-v3.05a-r8638-cygwin64\bin\wit.exe
+set "witRute=C:\wit-v3.05a-r8638-cygwin64\bin\wit.exe"
 
-if exist %witRute% (
-    ::/Do nothing here
+if exist "%witRute%" (
+    ::Do nothing here
     rem
-    ) else (
-    echo WIT has not located properly, be sure to edit the script var witRute to your wit installation.
+) else (
+    echo [INFO] WIT has not located properly. 
+	echo Edit the script line 2 to set witRute.
     echo To exit press enter.
     pause
     exit /b
-    )
-
+)
 echo Press start if you are ready to transform the files.
 pause
 
-if exist "out\" (
-  for %%F in (*.iso) do (
-      echo.
-      echo ----------------------------------------------------
-      echo Processing: "%%~nxF"
-      echo ----------------------------------------------------
-      :: Run the conversion command with quotes around paths
-      "%witRute%" copy "%%~fF" --dest "out\%%~nF.ciso" --ciso
-      if errorlevel 1 (
-        echo [ERROR] Failed to convert "%%~nxF"
-      ) else (
-        echo [SUCCESS] Converted to "out\%%~nF.ciso"
-        del "%%~nxF"
-      )
-  ) 
-
-
+:: Check if there are any ISOs to process
+if exist "*.iso" (
+    :: Check if the destination folder exists
+    if exist "out\" (
+        for %%F in (*.iso) do (
+            echo.
+            echo ----------------------------------------------------
+            echo Processing: "%%~nxF"
+            echo ----------------------------------------------------
+            
+            :: Run the conversion command with quotes around paths
+            "%witRute%" copy "%%~fF" --dest "out\%%~nF.ciso" --ciso
+            if errorlevel 1 (
+                echo [ERROR] Failed to convert "%%~nxF"
+            ) else (
+                echo [SUCCESS] Converted to "out\%%~nF.ciso"
+                del "%%~nxF"
+				)
+        )
+    ) else (
+        echo Folder Out does not exist, making it. The script will rerun.
+        mkdir Out
+        "%~nx0"
+    )
 ) else (
-  echo "Folder Out not exist, making it, the script will rerun on enter"
-  mkdir Out
-  "%~nx0"   
-
-) 
+    echo.
+    echo [INFO] No ISO files are found.
+    pause
+)
